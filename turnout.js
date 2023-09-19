77d8e3608ec636df0d47c8ee923c42ec14118a30
dev Zeus MAthew <devzeus.mathew721@gmail.com>
2023-09-19 22:01:55 +0530

    commited off finally

--- turnout.js
+++ turnout.js
@@ -14,7 +14,7 @@ always be in the range of
 integers.
 */
 
-const prompt = require("prompt-sync")();
+const prompt = require ("prompt-sync")({sigint: true})
 
 let length = parseInt(prompt("Enter the length of the rectangle:"));
 let breadth = parseInt(prompt("Enter the breadth of the rectangle:"));
